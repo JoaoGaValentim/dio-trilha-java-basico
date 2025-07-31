281617050c762bb0492e5984c3634d5955cf61df
João Gabriel valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-07-31 18:17:54 -0300

    added first module

--- java_variables/src/App.java
+++ java_variables/src/App.java
@@ -1,5 +1,16 @@
+import java.time.OffsetDateTime;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int age = 24;
+        double salary = 1599.5;
+        double salaryAdjustPercent = 0.5;
+        double newSalary = salary + (salary * salaryAdjustPercent);
+        // float temperature = 34.2F; // Java uses F to recognize float
+        // long distanceInYearsLight = 1222222L; // long use L
+        age = (OffsetDateTime.now().getYear() + 1) - 2001;
+        final float PI = 3.141516F;
+
+        System.err.printf("Idade %s e salário %.2f o valor de PI %.11f.\n", age, newSalary, PI);
     }
 }
